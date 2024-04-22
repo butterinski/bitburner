3ede218bf7bc5303c3f9144843575a700a3e17d2
butterinski <38936847+butterinski@users.noreply.github.com>
2024-04-21 21:43:13 -0500

    Update sleeve.js
    Sleeve.js now ignores sleeves that already have 0 Shock and 100 Sync

--- scripts/sleeve.js
+++ scripts/sleeve.js
@@ -1,27 +1,35 @@
 /** @param {NS} ns */
 
 export async function main(ns) {
+    
+    const sleeveList = [];
+    for (let a = 0; a < ns.sleeve.getNumSleeves(); ++a) {
+        if (ns.sleeve.getSleeve(a).sync < 100 || ns.sleeve.getSleeve(a).shock > 0) {
+            sleeveList.push(a);
+        }
+    }
+
     for (let c = 0; c < 3; ++c) {
-        for (let i = 0; i < ns.sleeve.getNumSleeves(); ++i) {
+        for (let i = 0; i < sleeveList.length; ++i) {
             switch (c) {
                 case 0:
-                    ns.sleeve.setToShockRecovery(i);
+                    ns.sleeve.setToShockRecovery(sleeveList[i]);
                     break;
                 case 1:
-                    while (ns.sleeve.getSleeve(i).shock > 0) {
-                        ns.print("Sleeve " + i + ": Shock = " + ns.sleeve.getSleeve(i).shock);
+                    while (ns.sleeve.getSleeve(sleeveList[i]).shock > 0) {
+                        ns.print("Sleeve " + sleeveList[i] + ": Shock = " + ns.sleeve.getSleeve(sleeveList[i]).shock);
                         await ns.sleep(600000);
                     }
-                    ns.sleeve.setToSynchronize(i);
-                    ns.print("Sleeve " + i + " set to Synchronize");
+                    ns.sleeve.setToSynchronize(sleeveList[i]);
+                    ns.print("Sleeve " + sleeveList[i] + " set to Synchronize");
                     break;
                 case 2:
-                    while (ns.sleeve.getSleeve(i).sync < 100) {
-                        ns.print("Sleeve " + i + ": Sync = " + ns.sleeve.getSleeve(i).sync);
+                    while (ns.sleeve.getSleeve(sleeveList[i]).sync < 100) {
+                        ns.print("Sleeve " + sleeveList[i] + ": Sync = " + ns.sleeve.getSleeve(sleeveList[i]).sync);
                         await ns.sleep(600000);
                     }
-                    ns.sleeve.setToCommitCrime(i, "Mug");
-                    ns.print("Sleeve " + i + " set to Mugging");
+                    ns.sleeve.setToBladeburnerAction(sleeveList[i], "Training");
+                    ns.print("Sleeve " + sleeveList[i] + " set to Training at Bladeburner");
                     break;
             }
         }
